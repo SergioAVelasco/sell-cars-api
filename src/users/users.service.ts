155069bcf502dbf7e054a8902ba0271ace3e2081
Sergio Armando <keko.vgs@gmail.com>
2025-04-20 22:25:27 -0600

    adding dabatases operations

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,4 +11,37 @@ export class UsersService {
 
     return this.repo.save(user);
   }
+
+  async findOne(id: number) {
+    const found = await this.repo.find({ where: { id } });
+    if (found.length === 0) {
+      throw new Error('User not found');
+    }
+    return found[0];
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    console.log('Updating user with id', id);
+    const user = await this.findOne(id);
+    console.log('User found', user);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    Object.assign(user, attrs);
+    console.log('User after update', user);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    console.log('Usuario', user);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return this.repo.remove(user);
+  }
 }
